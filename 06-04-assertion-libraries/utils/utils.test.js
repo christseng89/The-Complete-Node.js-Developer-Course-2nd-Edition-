03602c044cd6daeb16baf160c7ec1c11918d809d
Chris Tseng <samfire5200@gmail.com>
2018-05-04 16:45:31 +0800

    06-04

--- 06-04-assertion-libraries/utils/utils.test.js
+++ 06-04-assertion-libraries/utils/utils.test.js
@@ -4,8 +4,9 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-
   expect(res).toBe(44).toBeA('number');
+  var res = utils.add(33, 22);
+  expect(res).toBe(55).toBeA('number');
 });
 
 it('should square a number', () => {
@@ -19,10 +20,13 @@ it('should square a number', () => {
 it('should set firstName and lastName', () => {
   var user = {location: 'Philadelphia', age: 25};
   var res = utils.setName(user, 'Andrew Mead');
-
+  console.log(user);
   expect(res).toInclude({
     firstName: 'Andrew',
-    lastName: 'Mead'
+    lastName: 'Mead',
+    age: 25
+  }).toExclude({
+    age: 23
   });
 });
 
