3cbc403b8799b8f09b795f274d257d01aa4dd233
Chris Tseng <samfire5200@gmail.com>
2018-05-08 16:41:17 +0800

    07-13

--- 07-13-schemas/server/server.js
+++ 07-13-schemas/server/server.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
+  /* other options */
+});
 
 var Todo = mongoose.model('Todo', {
   text: {
