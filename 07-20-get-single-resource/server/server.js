db8c1bb1539bcdd60f7c26f583573791829659cc
Chris Tseng <samfire5200@gmail.com>
2018-05-10 14:30:32 +0800

    07-20

--- 07-20-get-single-resource/server/server.js
+++ 07-20-get-single-resource/server/server.js
@@ -32,6 +32,7 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+  console.log('id = ',id);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
