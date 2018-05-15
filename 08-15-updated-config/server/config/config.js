47d4ecb315067f83601454af0d2a46c2317a1063
Chris Tseng <samfire5200@gmail.com>
2018-05-15 17:51:44 +0800

    08-15

--- 08-15-updated-config/server/config/config.js
+++ 08-15-updated-config/server/config/config.js
@@ -3,6 +3,8 @@ var env = process.env.NODE_ENV || 'development';
 if (env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
+  // console.log('\n Env: . . . ', env, '\n', 'Config: . . . ', envConfig);
+  // console.log('\n Object Keys: . . ', Object.keys(envConfig));
 
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
