90d1d7c3784eb15782deff680b99e0e783f0263c
Chris Tseng <samfire5200@gmail.com>
2018-05-18 14:29:14 +0800

    09-13 moment

--- 09-13-momentjs/playground/time.js
+++ 09-13-momentjs/playground/time.js
@@ -2,16 +2,23 @@ var moment = require('moment');
 
 // Jan 1st 1970 00:00:10 am
 
-// var date = new Date();
+var date = '';
+// date = new Date();
 // var months = ['Jan', 'Feb']
 // console.log(date.getMonth());
 
-// var date = moment();
-// date.add(100, 'year').subtract(9, 'months');
-// console.log(date.format('MMM Do, YYYY'));
+date = moment();
+console.log(date.format());
+console.log(date.format('MMM Do, YYYY'));
+console.log(date.format('YYYY/MM/DD'));
+date.add(20, 'days');
+console.log(date.format('YYYY/MM/DD'));
+
+date.add(100, 'year').subtract(9, 'months');
+console.log(date.format('MMM Do, YYYY'));
 
 // 10:35 am
 // 6:01 am
 
-var date = moment();
-console.log(date.format('h:mm a'))
+// var date = moment();
+// console.log(date.format('h:mm a'))
