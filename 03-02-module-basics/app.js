45bae09b8db0ff5ac62af07c123584a5b5d53753
Chris Tseng <samfire5200@gmail.com>
2018-06-27 18:44:04 +0800

    03-02

--- 03-02-module-basics/app.js
+++ 03-02-module-basics/app.js
@@ -2,25 +2,19 @@ console.log('Starting app.')
 
 const fs = require('fs')
 const os = require('os')
+const dns = require('dns')
 
 const user = os.userInfo()
 // console.log(stringToHex(os.EOL))
 const carriageReturn = os.EOL
-//console.log(user)
-fs.appendFileSync('greetings.txt', `Hello ${user.username}! ${carriageReturn}`)
-fs.appendFileSync('greetings.txt', `Hostname ${os.hostname()}, Path ${os.homedir()} ${carriageReturn}`)
-//Get IP Address ...
-getIPAddress()
 
 function getIPAddress() {
-    require('dns').lookup(require('os').hostname(), function (err, addr, fam) {
+    dns.lookup(os.hostname(), function (err, addr, fam) {
         fs.appendFileSync('greetings.txt', `IP Address ${addr} ${carriageReturn}`)
     })
 }
-
-// function stringToHex(str) {
-//     //converting string into buffer
-//     let bufStr = Buffer.from(str, 'utf8')
-//     //with buffer, you can convert it into hex with following code
-//     return bufStr.toString('hex')
-// }
+//console.log(user)
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! ${carriageReturn}`)
+fs.appendFileSync('greetings.txt', `Hostname ${os.hostname()}, Path ${user.homedir} ${carriageReturn}`)
+//Get IP Address ...
+getIPAddress()
