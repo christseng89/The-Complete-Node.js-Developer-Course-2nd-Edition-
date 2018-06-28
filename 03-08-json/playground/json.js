6d369294a921df16e4f18f68dda60b66931466dc
Chris Tseng <samfire5200@gmail.com>
2018-06-28 15:58:40 +0800

    03-08

--- 03-08-json/playground/json.js
+++ 03-08-json/playground/json.js
@@ -1,46 +1,56 @@
 // #1 Convert Obj to Json
-// var obj = {
-//   name: ['Andrew','Chris \'Tseng']
-// };
-// var stringObj = JSON.stringify(obj);
-// console.log(typeof stringObj);
-// console.log(stringObj);
-// var parseObj = JSON.parse(stringObj);
-// console.log(parseObj.name[0]);
+
+console.log('Starting Step #1 。。。')
+var obj = {
+  name: ['Andrew', 'Chris \'Tseng']
+}
+var stringObj = JSON.stringify(obj)
+console.log(typeof stringObj)
+console.log(stringObj)
+var parseObj = JSON.parse(stringObj)
+console.log(parseObj)
+console.log(parseObj.name[0], '\n')
 
 // #2 Convert Json to Obj
-// var personString = '{"name": ["Andrew","Chris"], "age": [25,60]}';
-// var person = JSON.parse(personString);
-// console.log(typeof person);
-// console.log(person);
-// console.log(person.name[0], person.age[0]);
+console.log('Starting Step #2 。。。')
+var personString = '{"name": ["Andrew","Chris"], "age": [25,60]}'
+var person = JSON.parse(personString)
+console.log(typeof person)
+console.log(person)
+console.log(person.name[0], person.age[0], '\n')
 
 // #3 Convert Obj to Jason then Write into file.
-const fs = require('fs');
+const fs = require('fs')
 
 var originalNote = {
-  title: ['Some Title0','Title1', 'Title2'],
+  title: ['Some Title0', 'Title1', 'Title2'],
   body: ['Some Body0', 'Body1', 'Body2']
-};
-var originalNoteString = JSON.stringify(originalNote);
-fs.writeFileSync('notes.json', originalNoteString);
+}
+var originalNoteString = JSON.stringify(originalNote)
+fs.writeFileSync('notes.json', originalNoteString)
 
 // #4 Read from File in Json format, then convert it to Obj
-var noteString = fs.readFileSync('notes.json');
-var note = JSON.parse(noteString);
-console.log(typeof note, typeof note.title, note.title);
-var noteTitle = note.title;
+console.log('Starting Step #4 。。。')
+var noteString = fs.readFileSync('notes.json')
+var note = JSON.parse(noteString)
+console.log(typeof note, typeof note.title, note.title)
+const noteTitle = note.title
+const noteBody = note.body
 
 // #4.1 add filter for Object ....
-let noteFilter = noteTitle.filter(noteTitle1 => noteTitle1 === 'Title2');
-console.log('Filter #1', noteFilter);
+let noteFilter = noteTitle.filter(noteTitle1 => noteTitle1 === 'Title2')
+console.log(`Filter Title #1, Type of 'noteFilter[0]' is ${typeof noteFilter[0]} and 'noteFilter[0]' equals to ${noteFilter[0]}`)
 
-noteFilter = noteTitle.filter(noteTitle1 => noteTitle1 === 'Titlex');
-console.log('Filter #2', noteFilter);
+noteFilter = noteTitle.filter(noteTitle1 => noteTitle1 === 'Titlex')
+console.log('Filter Title #2', noteFilter[0])
+
+const bodyFilter = noteBody.filter(noteBody1 => noteBody1 === 'Body2')
+console.log('Filter Body #1', bodyFilter, bodyFilter[0], '\n')
 
 // #5 Object Filter Examples...
-// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present", "happy"];
-// console.log(typeof words);
-// let longWords = words.filter(word1 => word1.length > 6);
-// console.log(typeof longWords);
-// console.log(longWords);
+console.log('Starting Step #5 。。。')
+const words = ["spray", "limit", "elite", "exuberant", "destruction", "present", "happy"]
+console.log(typeof words)
+const longWords = words.filter(word1 => word1.length > 6)
+console.log(typeof longWords)
+console.log(`Words length greater than 6 are: ${longWords}`)
