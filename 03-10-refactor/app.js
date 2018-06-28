409369c0cd0c4e7e7b1237ae81da5b81e33cd8f8
Chris Tseng <samfire5200@gmail.com>
2018-06-28 20:18:31 +0800

    03-10

--- 03-10-refactor/app.js
+++ 03-10-refactor/app.js
@@ -1,32 +1,32 @@
-console.log('Starting app.js');
+console.log('Starting app.js')
 
-const fs = require('fs');
-const _ = require('lodash');
-const yargs = require('yargs');
+const fs = require('fs')
+const _ = require('lodash')
+const yargs = require('yargs')
 
-const notes = require('./notes.js');
+const notes = require('./notes.js')
 
-const argv = yargs.argv;
-var command = argv._[0];
-// console.log('Command: ', command);
-// console.log('Yargs', argv);
+const argv = yargs.argv
+var command = argv._[0]
+// console.log('Command: ', command)
+// console.log('Yargs', argv)
 
 if (command === 'add') {
-  var note = notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body)
   if (note) {
-    console.log('Note created');
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    console.log('Note created')
+    console.log('--')
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
   } else {
-    console.log(`Note title '${argv.title}' already exists`);
+    console.log(`Note title '${argv.title}' already exists`)
   }
 } else if (command === 'list') {
-  notes.getAll();
+  notes.getAll()
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  notes.getNote(argv.title)
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  notes.removeNote(argv.title)
 } else {
-  console.log('Command not recognized');
+  console.log('Command not recognized')
 }
