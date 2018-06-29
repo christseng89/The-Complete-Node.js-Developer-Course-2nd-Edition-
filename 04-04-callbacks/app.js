e40caf7f9c4728ac52c0de31b96c31afc0aa1137
Chris Tseng <samfire5200@gmail.com>
2018-06-29 19:03:05 +0800

    04-04

--- 04-04-callbacks/app.js
+++ 04-04-callbacks/app.js
@@ -1,23 +1,23 @@
-const request = require('request');
+const request = require('request')
 
-function requestBody (addr) {
+const requestBody = (addr) => {
   request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+addr,
+    url: `https://console.developers.google.com/apis/credentials?project=storied-polymer-203509`,
     json: true
   }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2),'\n');
-  });
+    // console.log(JSON.stringify(body, undefined, 2),'\n')
+  }) 
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}`,
+    json: true
+  }, (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2),'\n')
+  })
 }
-// request({
-//   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=玄武大道 南京',
-//   json: true
-// }, (error, response, body) => {
-//   console.log(body);
-// });
 
-console.log('Before call back...');
-requestBody('1301 lombard street philadelphia');
-
-requestBody('Xuan Wu DaDao Nanjing');
-console.log('After call back...');
+console.log('Call back start...')
+requestBody('1301 lombard street philadelphia')
+requestBody('Xuan Wu DaDao Nanjing')
+console.log('End of program...')
 
--- 04-04-callbacks/playground/callbacks.js
+++ 04-04-callbacks/playground/callbacks.js
@@ -2,18 +2,18 @@ var getUser = (id, callback) => {
   var user = {
     id: id,
     name: 'Vikram'
-  };
+  }
 
-  console.log("Callback w/o timeout");
-  callback(user);
+  console.log("Callback w/o timeout")
+  callback(user)
 
   setTimeout(() => {
-    callback(user);
-    console.log("Callback w timeout...");
-  }, 3000);
-  console.log("Hi!");
-};
+    callback(user)
+    console.log("Callback w timeout...")
+  }, 3000)
+  console.log("Hi!")
+}
 
 getUser(31, (userObject) => {
-  console.log(userObject);
-});
+  console.log(userObject)
+})
