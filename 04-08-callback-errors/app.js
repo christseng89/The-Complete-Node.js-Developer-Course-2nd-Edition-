a6ff60e2cb6c93b6d4c8ff6260e53fb0c64bc664
Chris Tseng <samfire5200@gmail.com>
2018-07-01 17:57:03 +0800

    04-08

--- 04-08-callback-errors/app.js
+++ 04-08-callback-errors/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
-const yargs = require('yargs');
+const request = require('request')
+const yargs = require('yargs')
 
 const argv = yargs
   .options({
@@ -12,21 +12,21 @@ const argv = yargs
   })
   .help()
   .alias('help', 'h')
-  .argv;
+  .argv
 
-var encodedAddress = encodeURIComponent(argv.address);
+var encodedAddress = encodeURIComponent(argv.address)
 
 request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   if (error) {
-    console.log('Unable to connect to Google servers.');
+    console.log('Unable to connect to Google servers.')
   } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
+    console.log('Unable to find that address.')
   } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
   }
-});
+})
