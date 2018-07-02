0842f9e148f9e6e72158ecb2103cca80bb2ae19e
Chris Tseng <samfire5200@gmail.com>
2018-07-02 11:32:14 +0800

    04-10

--- 04-10-weather/app.js
+++ 04-10-weather/app.js
@@ -1,6 +1,6 @@
-// const yargs = require('yargs');
+// const yargs = require('yargs')
 //
-// const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode')
 //
 // const argv = yargs
 //   .options({
@@ -13,33 +13,33 @@
 //   })
 //   .help()
 //   .alias('help', 'h')
-//   .argv;
+//   .argv
 //
 // geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 //   if (errorMessage) {
-//     console.log(errorMessage);
+//     console.log(errorMessage)
 //   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
+//     console.log(JSON.stringify(results, undefined, 2))
 //   }
-// });
+// })
 
 // 4a04d1c42fd9d32c97a2c291a32d5e2d
 
-const request = require('request');
+const request = require('request')
 
 request({
   url: 'https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/39.9396284,-75.18663959999999',
   json: true
 }, (error, response, body) => {
   if (error) {
-    console.log('Unable to connect to Forecast.io server.');
+    console.log('Unable to connect to Forecast.io server.')
   } else if (response.statusCode === 400) {
-    console.log('Unable to fetch weather.');
+    console.log('Unable to fetch weather.')
   } else if (response.statusCode === 200) {
-    console.log(body.currently.temperature);
+    console.log(body.currently.temperature)
   } else {
-    console.log('Unable to fetch weather for unknown reason.');    
+    console.log('Unable to fetch weather for unknown reason.')    
   }
-});
+})
 
-console.log('Test...');
+console.log('Test...')
--- 04-10-weather/playground/callbacks.js
+++ 04-10-weather/playground/callbacks.js
@@ -2,13 +2,13 @@ var getUser = (id, callback) => {
   var user = {
     id: id,
     name: 'Vikram'
-  };
+  }
 
   setTimeout(() => {
-    callback(user);
-  }, 3000);
-};
+    callback(user)
+  }, 3000)
+}
 
 getUser(31, (userObject) => {
-  console.log(userObject);
-});
+  console.log(userObject)
+})
