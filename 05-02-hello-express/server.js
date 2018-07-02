a8255dc2582ab86c1a8593642e0a92201dbb904b
Chris Tseng <samfire5200@gmail.com>
2018-07-02 13:17:27 +0800

    05-02

--- 05-02-hello-express/server.js
+++ 05-02-hello-express/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
 
-var app = express();
+var app = express()
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
+  // res.send('<h1>Hello Express!</h1>')
   res.send({
     name: 'Andrew',
     likes: [
@@ -12,18 +12,18 @@ app.get('/', (req, res) => {
       'Sports',
       'Music'
     ]
-  });
-});
+  })
+})
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
-});
+  res.send('About Page')
+})
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
-  });
-});
+  })
+})
 
-app.listen(3000);
+app.listen(3000)
