fa4fc69e002be911c0179e1eed29dd2a66bc67da
Chris Tseng <samfire5200@gmail.com>
2018-07-02 13:23:48 +0800

    05-03

--- 05-03-static-server/server.js
+++ 05-03-static-server/server.js
@@ -1,31 +1,32 @@
-const express = require('express');
+const express = require('express')
 
-var app = express();
+var app = express()
 
-app.use(express.static(__dirname + '/public'));
+// http://localhost:3000/help.html
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
+  // res.send('<h1>Hello Express!</h1>')
   res.send({
     name: 'Andrew',
     likes: [
       'Biking',
       'Cities'
     ]
-  });
-});
+  })
+})
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
-});
+  res.send('About Page')
+})
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
-  });
-});
+  })
+})
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000');
-});
+  console.log('Server is up on port 3000')
+})
