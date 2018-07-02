fb1e3e2dff17b6010a7a0f35bb98bddc478c8aa6
Chris Tseng <samfire5200@gmail.com>
2018-07-02 11:48:11 +0800

    04-12

--- 04-12-promises/playground/promise.js
+++ 04-12-promises/playground/promise.js
@@ -1,28 +1,28 @@
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve('Hey. It worked!');
-    reject('Unable to fulfill promise');
-  }, 2500);
-});
+    // resolve('Hey. It worked!')
+    reject('Unable to fulfill promise')
+  }, 2000)
+})
 
-console.log(somePromise);
+// console.log(somePromise)
 
 somePromise.then((message) => {
-  console.log('Success: ', message);
+  console.log('Success: ', message)
 }, (errorMessage) => {
-  console.log('Error: ', errorMessage);
-});
+  console.log('Error: ', errorMessage)
+})
 
 // var promise1 = new Promise(function (resolve, reject) {
-//   setTimeout(resolve, 100, 'foo');
-// });
+//   setTimeout(resolve, 100, 'foo')
+// })
 
 // promise1.then((message) => {
-//   console.log('Success:', message);
+//   console.log('Success:', message)
 // },
 // (errorMessage) => {
-//   console.log('Fail:', errorMessage);
-// });
+//   console.log('Fail:', errorMessage)
+// })
 
-// console.log(promise1);
+// console.log(promise1)
 // expected output: [object Promise]
